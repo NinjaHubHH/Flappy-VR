e4c49bff080cc93d060b4f445a52dc4739112d62
Ninja <nruesch@live.de>
2016-04-07 13:51:57 +0200

    no message

---
+++ Assets/_MyAssets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	Vector3 forceUp = new Vector3(0,200,0);
+	Vector3 resetVector = new Vector3 (0, 0, 0);
+	Rigidbody playerRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		playerRigidbody = GetComponent<Rigidbody> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+
+			playerRigidbody.velocity = resetVector;
+			playerRigidbody.AddForce (forceUp);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision){
+
+		Die();
+	}
+
+	void Die(){
+
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
